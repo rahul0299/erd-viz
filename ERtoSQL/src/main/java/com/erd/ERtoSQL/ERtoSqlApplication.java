27890941e793a94830e922fda99d13f171b925cd
Ezio99 <vaibhavritam@gmail.com>
2024-03-26 19:29:05 +0800

    Added Spring boot app

---
+++ ERtoSQL/src/main/java/com/erd/ERtoSQL/ERtoSqlApplication.java
@@ -0,0 +1,13 @@
+package com.erd.ERtoSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERtoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERtoSqlApplication.class, args);
+	}
+
+}
---
+++ ERtoSQL/src/test/java/com/erd/ERtoSQL/ERtoSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erd.ERtoSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERtoSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
