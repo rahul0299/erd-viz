9a2d047405ed3e8f570984d7fd8f010391e4c14e
Ezio99 <vaibhavritam@gmail.com>
2024-03-31 20:53:41 +0800

    Merging logic and table creation

--- ERtoSQL/src/main/java/com/erd/ERtoSQL/domain/ERElements/Entity.java
+++ ERtoSQL/src/main/java/com/erd/ERtoSQL/domain/ERElements/Entity.java
@@ -10,7 +10,10 @@ public class Entity extends Node {
 
 
     Boolean isWeak = false;
+    // Lets you know that it has to merge with something
     String mergeWith;
+    // Lets you know if it has already been merged with some table created and the reference of that table
+    String tableCreated;
 
     public Entity(String name, String primaryKey, List<Object> attributes) {
         super(name, primaryKey, attributes);
--- ERtoSQL/src/main/java/com/erd/ERtoSQL/service/ConversionService.java
+++ ERtoSQL/src/main/java/com/erd/ERtoSQL/service/ConversionService.java
@@ -8,6 +8,7 @@ import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -23,6 +24,15 @@ public class ConversionService {
 
     HashMap<String, HashMap<String, List<String>>> relationInfo = new HashMap<>();
 
+    HashMap<String,Table> tablesCreated = new HashMap<>();
+
+    private final String mergeWith = "MergeWith";
+    private final String mergeWithWeak = "MergeWithWeak";
+    private final String primaryKeyException = "PrimaryKeyException";
+    private final String attachedToEntities = "AttachedToEntities";
+
+    private final String normalCase = "NormalCase";
+
 
     public String handleErToSQL(String erData) {
         JSONObject jsonObject = new JSONObject(erData);
@@ -33,7 +43,7 @@ public class ConversionService {
 
         findInfoForRelations();
 
-//        addAttributes();
+        iterateOverNodesToCreateTables();
 
         return entityMap.toString();
     }
@@ -101,31 +111,116 @@ public class ConversionService {
             }
 
             relationInfo.put(relation, new HashMap<>());
-            relationInfo.get(relation).put("MergeWith", new ArrayList<>());
-            relationInfo.get(relation).put("MergeWithWeak", new ArrayList<>());
-            relationInfo.get(relation).put("PrimaryKeyException", new ArrayList<>());
-            relationInfo.get(relation).put("AttachedToEntities", new ArrayList<>());
+            relationInfo.get(relation).put(mergeWith, new ArrayList<>());
+            relationInfo.get(relation).put(mergeWithWeak, new ArrayList<>());
+            relationInfo.get(relation).put(primaryKeyException, new ArrayList<>());
+            relationInfo.get(relation).put(attachedToEntities, new ArrayList<>());
+            relationInfo.get(relation).put(normalCase, new ArrayList<>());
 
 
             for (MutablePair<String, Link> pair : linksMap.get(relation)) {
+
+                relationInfo.get(relation).get(attachedToEntities).add(pair.getLeft());
+
                 if (entityMap.get(pair.getLeft()) != null && entityMap.get(pair.getLeft()).getIsWeak()) {
                     //Merge
                     entityMap.get(pair.getLeft()).setMergeWith(relation);
-                    relationInfo.get(relation).get("MergeWithWeak").add(pair.getLeft());
+                    relationInfo.get(relation).get(mergeWithWeak).add(pair.getLeft());
 
                 } else if (pair.getRight().getLowerBound().equalsIgnoreCase("1") &&
                         pair.getRight().getUpperBound().equalsIgnoreCase("1")) {
                     //Merge
                     entityMap.get(pair.getLeft()).setMergeWith(relation);
-                    relationInfo.get(relation).get("MergeWith").add(pair.getLeft());
+                    relationInfo.get(relation).get(mergeWith).add(pair.getLeft());
                 } else if (pair.getRight().getLowerBound().equalsIgnoreCase("0") &&
                         pair.getRight().getUpperBound().equalsIgnoreCase("1")) {
                     //Special case
-                    relationInfo.get(relation).get("PrimaryKeyException").add(pair.getLeft());
+                    relationInfo.get(relation).get(primaryKeyException).add(pair.getLeft());
+                    relationInfo.get(relation).get(normalCase).add(pair.getLeft());
+                }
+                else{
+                    relationInfo.get(relation).get(normalCase).add(pair.getLeft());
                 }
             }
         }
 
+    }
+
+    private void iterateOverNodesToCreateTables(){
+        for(String entityKey: entityMap.keySet()){
+            if(entityMap.get(entityKey).getMergeWith()!=null){
+                continue;
+            }
+
+            Entity currentEntity = entityMap.get(entityKey);
+            //create table
+            tablesCreated.put(entityKey,new Table(currentEntity.getName(),currentEntity.getAttributes()
+                    , Arrays.asList(currentEntity.getPrimaryKey()),null));
+
+            currentEntity.setTableCreated(entityKey);
+        }
+
+        for(String relationKey: relationMap.keySet()){
+            if(relationInfo.get(relationKey).get(mergeWith).isEmpty() &&
+                    relationInfo.get(relationKey).get(mergeWithWeak).isEmpty()){
+                continue;
+            }
+
+            if(!relationInfo.get(relationKey).get(mergeWith).isEmpty()){
+                Table table;
+                Relation currentRelation = relationMap.get(relationKey);
+                String name = currentRelation.getName();
+                List<String> attributes = new ArrayList<>(currentRelation.getAttributes());
+                List<String> primaryKey = new ArrayList<>();
+                //Add key of table not in merge relation but in attached
+                List<MutablePair<String, String>> foreignKeys = new ArrayList<>();
+
+                for(String entityKey : relationInfo.get(relationKey).get(normalCase)){
+                    foreignKeys.add(new MutablePair<>(entityMap.get(entityKey).getPrimaryKey(),entityMap.get(entityKey).getName()));
+                    attributes.addAll(entityMap.get(entityKey).getAttributes());
+                }
+
+                //R table ready
+                String finalMergeWith=null;
+                for(String entityKey: relationInfo.get(relationKey).get(mergeWith)){
+                    if(entityMap.get(entityKey).getTableCreated()!=null){
+                        //Merge with table with reference entityKey in tablesCreated
+                        //Ignore
+                        finalMergeWith = entityMap.get(entityKey).getTableCreated();
+                    }
+                    else{
+                        //Merge with entity directly and keep merging with other entities and put it finally in tablesCreated
+                        Entity currentEntity = entityMap.get(entityKey);
+                        name = currentEntity.getName() + "_"+name;
+                        attributes.addAll(currentEntity.getAttributes());
+                        primaryKey.add(currentEntity.getPrimaryKey());
+
+                    }
+                }
+                if(finalMergeWith==null){
+                    tablesCreated.put(relationInfo.get(relationKey).get(mergeWith).get(0),new Table(name,attributes,primaryKey,foreignKeys));
+                    relationInfo.get(relationKey).get(mergeWith).forEach(t-> entityMap.get(t).setTableCreated(relationInfo.get(relationKey).get(mergeWith).get(0)));
+                }
+                else{
+                    //Add it to finalMergeWith
+                    Table currentTable = tablesCreated.get(finalMergeWith);
+                    currentTable.setName(currentTable.getName()+name);
+                    currentTable.getAttributes().addAll(attributes);
+                    currentTable.getPrimaryKey().addAll(primaryKey);
+                    currentTable.getForeignKeys().addAll(foreignKeys);
+                }
+            }
+
+        }
+
+        for(String relationKey: relationMap.keySet()){
+            if(!relationInfo.get(relationKey).get(mergeWith).isEmpty() ||
+                    !relationInfo.get(relationKey).get(mergeWithWeak).isEmpty()){
+                continue;
+            }
+
+            //Create table
+        }
     }
 }
 
