35d102aef38c545de49626a358efa4d705eaddef
rahul0299 <rahulbhargava1999@gmail.com>
2024-04-02 19:13:40 +0800

    adding in axios request to backend

--- react-app/erd-viz/src/components/PreviewModal/PreviewModal.jsx
+++ react-app/erd-viz/src/components/PreviewModal/PreviewModal.jsx
@@ -11,19 +11,46 @@ import {
 import {Close} from "@mui/icons-material";
 import {useEffect, useState} from "react";
 import { saveAs } from 'file-saver';
+import axios from "axios";
 
 const PreviewModal = (props) => {
 
     const [content, setContent] = useState(null);
 
     useEffect(() => {
-        setContent(null);
-        setTimeout(() => setContent(props.model), 2000);
-    }, [props.model]);
+        if (props.open) {
+            setContent(null);
+
+            const data = props.model;
+
+            const config = {
+                method: 'post',
+                maxBodyLength: Infinity,
+                url: 'https://ertosql-latest.onrender.com/toSQL',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                },
+                data : data
+            };
+
+            axios.request(config)
+                .then((response) => {
+                    setContent(response.data.sql);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+
+    }, [props.open]);
 
     const saveContent = () => {
         if (content != null) {
-            const blob = new Blob([JSON.stringify(content)], {type: "text/plain;charset=utf-8"});
+            let str = "";
+            content.forEach(line => str += '\n' + line);
+            console.log(str)
+            const blob = new Blob([str], {type: "text/plain;charset=utf-8"});
             saveAs(blob, 'schema.sql');
         }
     }
@@ -56,9 +83,7 @@ const PreviewModal = (props) => {
                     // eslint-disable-next-line react/prop-types
                     content
                         ?
-                        <Typography>
-                            {JSON.stringify(content)}
-                        </Typography>
+                        content.map(c => <Typography>{c}</Typography>)
                         :
                         <Stack spacing={1}>
                             <Skeleton variant="rectangular" height={90} />
